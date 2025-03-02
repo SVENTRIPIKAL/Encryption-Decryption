16d7992c652b20aa7bd43ae95a659d52e84c7623
Jose Garza <sventripikal@gmail.com>
2025-03-02 02:30:52 -0600

    ENCRYPT/DECRYPT FUNCTION
    -updated function to use basic left/right shifts for all characters in string
    -added lambda to function for simplicity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,27 @@
-fun main() = println(encryptWithKey("welcome to hyperskill", 5))
+fun main() {
+    println(encryptDecryptWithKey(
+        "enc",
+        "Welcome to hyperskill!",
+        5
+    ))
+}
 
 /**
  *  returns a new string with each
  *  character shifted a number of times
- *  to the right according to the key value.
- *  Incrementing restarts at 'a' after 'z'.
- *  Keeps punctuations & spaces unedited
- *  @param string string message to encrypt with cypher
- *  @param key determines the number of Right shifts
+ *  to the Left/Right according to the
+ *  provided Mode & Key value
+ *  @param mode string value deciding encryption/decryption
+ *  @param message string message to encrypt/decrypt with cypher
+ *  @param key determines the number of Left/Right shifts
  */
-private fun encryptWithKey(string: String, key: Int): String {
-    // create alphabet range
-    val alphabetRange = ('a'..'z')
-    // loop through string & create lowercase output
-    var output = ""
-    for (char in string.lowercase()) {
-        // update output
-        output += when {
-            // keep non-letters unedited
-            char !in alphabetRange -> char
-            // add key shift & subtract 26 if greater than range
-            else -> {
-                var shifted = char + key
-                if (shifted > alphabetRange.last) shifted -= 26
-                shifted
-            }
+private fun encryptDecryptWithKey(mode: String, message: String, key: Int): String {
+    val logic = { char: Char ->
+        when (mode) {
+            "enc" -> char + key
+            "dec" -> char - key
+            else -> char
         }
     }
-    return output
+    return message.map { logic(it) }.joinToString("")
 }

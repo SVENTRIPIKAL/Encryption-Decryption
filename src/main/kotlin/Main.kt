954eb43b29e4baaa738158860305578865b4e024
Jose Garza <sventripikal@gmail.com>
2025-03-02 18:53:19 -0600

    ENCRYPT/DECRYPT CLI FUNCTION
    -created function to group cli flags for main function arguments
    -added try/catch blocks to account for exceptions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
-fun main() {
-    println(encryptDecryptWithKey(
-        "enc",
-        "Welcome to hyperskill!",
-        5
-    ))
+const val ASTERISK = "*"
+const val ILLEGAL_INPUT = "Illegal Flag Argument - for \"*\""
+
+fun main(args: Array<String>) {
+    try {
+        when {
+            args.isEmpty() -> throw IllegalArgumentException(
+                ILLEGAL_INPUT.replace(ASTERISK, "Empty Array")
+            )
+            else -> {
+                val arguments = groupCliArguments(args)
+                println(encryptDecryptWithKey(
+                    mode = arguments["mode"]!!,
+                    key = arguments["key"]!!.toInt(),
+                    data = arguments["data"]!!
+                ))
+            }
+        }
+    } catch (e: Exception) {
+        println(e.localizedMessage)
+    }
 }
 
 /**
@@ -12,10 +27,10 @@ fun main() {
  *  to the Left/Right according to the
  *  provided Mode & Key value
  *  @param mode string value deciding encryption/decryption
- *  @param message string message to encrypt/decrypt with key
  *  @param key determines the number of Left/Right shifts
+ *  @param data string message to encrypt/decrypt with key
  */
-private fun encryptDecryptWithKey(mode: String, message: String, key: Int): String {
+private fun encryptDecryptWithKey(mode: String, key: Int, data: String): String {
     val logic = { char: Char ->
         when (mode) {
             "enc" -> char + key
@@ -23,5 +38,44 @@ private fun encryptDecryptWithKey(mode: String, message: String, key: Int): Stri
             else -> char
         }
     }
-    return message.map { logic(it) }.joinToString("")
+    return data.map { logic(it) }.joinToString("")
+}
+
+/**
+ *  organizes & returns a map of arguments
+ *  passed into the program for the following
+ *  flags: -mode, -key, -data
+ *  @param args string array storing program arguments
+ *  @throws IllegalArgumentException
+ */
+private fun groupCliArguments(args: Array<String>): Map<String, String> {
+    // map for flag & argument values
+    val map = mutableMapOf<String, String>()
+
+    // lambda flag check
+    val isNotFlag = { s: String -> s !in listOf("-mode", "-key", "-data") }
+
+    // organize arguments
+    for (i in args.indices step 2) {
+        // check index in range
+        if (i.inc() != args.lastIndex.inc()) {
+            // current index
+            when (args[i]) {
+                "-mode" -> {    // check next index not flag & update with index else default
+                    map["mode"] = if (isNotFlag(args[i.inc()])) args[i.inc()] else "enc"
+                }
+                "-key" -> {    // check next index not flag & update with index else default
+                    map["key"] = if (isNotFlag(args[i.inc()]) &&
+                        Character.isDigit(args[i.inc()].first())
+                    ) args[i.inc()] else "0"
+                }
+                "-data" -> {    // check next index not flag & update with index else default
+                    map["data"] = if (isNotFlag(args[i.inc()])) args[i.inc()] else ""
+                }               // throw exception for all other cases
+                else -> throw IllegalArgumentException(ILLEGAL_INPUT.replace(ASTERISK, args[i]))
+            }
+        }
+    }
+    // return arguments
+    return map
 }

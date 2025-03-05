acc46516210d294d8f0fb10cf8dde3fc647a81c7
Jose Garza <sventripikal@gmail.com>
2025-03-05 16:06:52 -0600

    ALGO ARGUMENT
    -added algo argument to allow user to choose between shift & unicode encryption styles

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,7 @@ fun main(args: Array<String>) {
     try {
         val arguments = parseCliArguments(args)
         parseEncryptDecrypt(
+            alg = arguments["alg"]!!,
             mode = arguments["mode"]!!,
             key = arguments["key"]!!.toInt(),
             data = arguments["data"]!!,
@@ -23,29 +24,33 @@ fun main(args: Array<String>) {
 /**
  *  organizes & returns a map of arguments
  *  passed into the program for the following
- *  flags: -mode, -key, -data, -in, -out
+ *  flags: -alg, -mode, -key, -data, -in, -out
  *  @param args string array storing program arguments
  *  @throws IllegalArgumentException
  */
 private fun parseCliArguments(args: Array<String>): Map<String, String> {
     // check if array empty or contains blank arguments
     when {
-        args.size !in 6..10 -> throw IllegalArgumentException(
+        args.size !in 6..12 -> throw IllegalArgumentException(
             ILLEGAL_INPUT.replace(ASTERISK, ARGS_NUMBER)
         )
         args.contains("") -> throw IllegalArgumentException(
             ILLEGAL_INPUT.replace(ASTERISK, ARGS_MISSING)
         )
     }
-    // map for flag & argument values
+    // map default argument values
     val map = mutableMapOf(
-        "mode" to "enc", "key" to "0", "data" to "", "in" to "", "out" to ""
+        "alg" to "shift", "mode" to "enc", "key" to "0",
+        "data" to "", "in" to "", "out" to ""
     )
     // trim array arguments
     val array = args.map { it.trim() }.toTypedArray()
 
     // lambda flag check
-    val isNotFlag = { f: String -> f !in listOf("-mode", "-key", "-data", "-in", "-out") }
+    val isNotFlag = { f: String -> f !in listOf("-alg", "-mode", "-key", "-data", "-in", "-out") }
+
+    // lambda algorithm check
+    val isValidAlgo = { a: String -> a in listOf("shift", "unicode") }
 
     // lambda mode check
     val isValidMode = { m: String -> m in listOf("enc", "dec") }
@@ -61,6 +66,9 @@ private fun parseCliArguments(args: Array<String>): Map<String, String> {
         // current & next indices
         val currentI = array[i]; val nextI = array[i.inc()]
         when (currentI) {
+            "-alg" -> {    // check next index valid algo else default
+                map["alg"] = checkIfElse(isValidAlgo, nextI, "shift")
+            }
             "-mode" -> {    // check next index valid mode else default
                 map["mode"] = checkIfElse(isValidMode, nextI, "enc")
             }
@@ -88,7 +96,8 @@ private fun parseCliArguments(args: Array<String>): Map<String, String> {
  *  prints/writes a new string with each
  *  character shifted a number of times
  *  to the Left/Right according to the
- *  provided Mode & Key value
+ *  provided Algorithm, Mode, & Key value
+ *  @param alg encryption/decryption style: shift/unicode
  *  @param mode string value deciding encryption/decryption
  *  @param key determines the number of Left/Right shifts
  *  @param data string message to encrypt/decrypt with key
@@ -97,6 +106,7 @@ private fun parseCliArguments(args: Array<String>): Map<String, String> {
  *  @throws NoSuchFileException
  */
 private fun parseEncryptDecrypt(
+    alg: String,
     mode: String,
     key: Int,
     data: String,
@@ -104,8 +114,7 @@ private fun parseEncryptDecrypt(
     outFile: String
 ) {
     // default read/write path
-    val defaultPath = System.getProperty("user.dir") +
-            "${File.separator}.test${File.separator}"
+    val defaultPath = System.getProperty("user.dir") + "${File.separator}.test${File.separator}"
 
     // regex capturing file + extension
     val fileFormatRegex = "^\\w+([.][A-Za-z0-9]{3,8})".toRegex()
@@ -113,22 +122,47 @@ private fun parseEncryptDecrypt(
     // lambda checking if string is valid file format
     val isValidFormat = { s: String -> s.matches(fileFormatRegex) }
 
-    // lambda encrypt/decrypt by mode
+    // lambda encrypt/decrypt by mode + algo
     val logic = { char: Char ->
-        when (mode) {
-            "enc" -> char + key
-            "dec" -> char - key
+        when (alg) {
+            "shift" -> {
+                val alphaRange = 'a'..'z'
+                val lowerCased = char.lowercase().first()
+                if (lowerCased !in alphaRange) char
+                else {
+                    when (mode) {
+                        "enc" -> {
+                            val temp = lowerCased + key
+                            if (temp > alphaRange.last) char + key - 26
+                            else char + key
+                        }
+                        "dec" -> {
+                            val temp = lowerCased - key
+                            if (temp < alphaRange.first) char - key + 26
+                            else char - key
+                        }
+                        else -> char
+                    }
+                }
+            }
+            "unicode" -> {
+                when (mode) {
+                    "enc" -> char + key
+                    "dec" -> char - key
+                    else -> char
+                }
+            }
             else -> char
         }
     }
+
     // lambda returns string encrypted
     val encrypt = { s: String -> s.map { c -> logic(c) }.joinToString("") }
 
     // check if data blank else assign infile value
     val inputData = data.ifBlank { inFile }
 
-    when {
-        // input is valid file format
+    when { // input is valid file format
         isValidFormat(inputData) -> {
             // create reader for in-file
             val reader = File("${defaultPath}$inputData")
@@ -137,7 +171,7 @@ private fun parseEncryptDecrypt(
                     // create writer for out-file
                     val writer = File("${defaultPath}$outFile")
                     // create empty file
-                    writer.writeText("")
+                    writer.createNewFile()
                     // read from in-file & write to out-file
                     reader.useLines {
                         it.forEach {  line ->
@@ -147,8 +181,7 @@ private fun parseEncryptDecrypt(
                 }   // out-file empty/incorrectly formatted, so print to screen encrypted
                 else -> reader.forEachLine { println(encrypt(it)) }
             }
-        }
-        // input data contains regular string data
+        }   // input data contains string data
         inputData.isNotBlank() -> {
             when {  // out-file is a valid file format
                 isValidFormat(outFile) -> {
@@ -160,6 +193,10 @@ private fun parseEncryptDecrypt(
                 }   // out-file empty/incorrectly formatted, so print to screen encrypted
                 else -> println(encrypt(inputData))
             }
+        }   // input blank
+        else -> {   // create file if out-file is a valid file format
+            if (isValidFormat(outFile)) File("${defaultPath}$outFile").createNewFile()
+            else println(inputData)
         }
     }
 }

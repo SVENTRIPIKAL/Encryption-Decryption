634b6b5b75acf05b9b4bcfbfe5eabf000b6890e7
Jose Garza <sventripikal@gmail.com>
2025-03-05 18:04:03 -0600

    CONSTANTS FILE
    -added constant file
    -added & replaced all constants in functions

---
+++ src/main/kotlin/CONSTANTS.kt
@@ -0,0 +1,32 @@
+// numbers
+const val ZERO = 0
+const val TWO = 2
+const val SIX = 6
+const val TWELVE = 12
+const val TWO_SIX = 26
+
+// exception errors
+const val ASTERISK = "*"
+const val EMPTY_STRING = ""
+const val ERROR_MISSING = "Missing Arguments"
+const val ERROR_NUMBER = "Number of Arguments"
+const val ILLEGAL_INPUT = "IllegalArgument Error - for \"*\""
+
+// Main.parseCliArguments
+// arguments
+const val ARG_ALG = "alg"
+const val ARG_MODE = "mode"
+const val ARG_KEY = "key"
+const val ARG_DATA = "data"
+const val ARG_IN = "in"
+const val ARG_OUT = "out"
+// algorithms
+const val ALG_SHIFT = "shift"
+const val ALG_UNICODE = "unicode"
+// modes
+const val MODE_ENC = "enc"
+const val MODE_DEC = "dec"
+
+// Main.parseEncryptDecrypt
+const val USER_DIR = "user.dir"
+const val FILE_EXT_REGEX = "^\\w+([.][A-Za-z0-9]{3,8})"
